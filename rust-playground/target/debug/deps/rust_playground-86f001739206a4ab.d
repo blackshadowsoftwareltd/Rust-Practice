b733947204417ed463a2e7347e5ed7a46fd785c1
blackshadowdoftwareltd <blackshadowsoftwareltd.com>
2023-03-11 22:57:42 +0600

    init playground

---
+++ rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.d
@@ -0,0 +1,5 @@
+/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.rmeta: src/main.rs
+
+/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.d: src/main.rs
+
+src/main.rs:
---
+++ rust-playground/target/debug/deps/rust_playground-b0b6e89540384f06.d
@@ -0,0 +1,5 @@
+/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-b0b6e89540384f06.rmeta: src/main.rs
+
+/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-b0b6e89540384f06.d: src/main.rs
+
+src/main.rs:
