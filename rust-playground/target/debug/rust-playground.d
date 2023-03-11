f8ca1448ccaa36d00ab83ffca4c084e80d97ecd8
remon <blackshadowsoftwareltd@gmail.com>
2023-03-12 00:35:56 +0600

    test on fedora

--- rust-playground/target/debug/rust-playground.d
+++ rust-playground/target/debug/rust-playground.d
@@ -1,1 +1,1 @@
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/rust-playground: /home/remon/Rusty/Rust-Practice/rust-playground/src/main.rs
+/home/remon/Projects/Go\ Project/Rust-Practice/rust-playground/target/debug/rust-playground: /home/remon/Projects/Go\ Project/Rust-Practice/rust-playground/src/main.rs
