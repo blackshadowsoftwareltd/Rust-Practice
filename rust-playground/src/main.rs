510fb8dd5055a76f0f46bcc05b5c79c0b7f1e605
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-12 01:46:26 +0600

    Print Output

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,5 +1,28 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Hello, world!");
+    //? print example of rust lang
+    println!("Hellow world");
+    print!("Hello, world!");
+    println!();
+
+    let name = "Remon";
+    let age = 23;
+    //? Here, {} is a placeholder which is replaced by the value of the variable after the comma. That's why we get 31 as output instead of {}
+    println!("{}", age);
+
+    println!("Age : {}", age); // {} is a placeholder
+
+    //? Print multiple variable
+    println!("Name : {}, Age : {}", name, age);
+
+    //? specify the numbering for placeholders to print variables in different order.
+    //? name is 0 index & age is 1 index
+    println!("Age : {1}, Name : {0}", name, age);
+
+    //?also use the variable names directly inside the placeholder.
+    println!("Name : {name}, Age : {age}");
+
+    //? Print Newline
+    print!("Rust is fun!\nI love Rust programming.");
 }
