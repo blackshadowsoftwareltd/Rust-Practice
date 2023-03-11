e8ac4f9ef3d4297d97be5a3b253d562f2dc65ea1
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-12 02:17:11 +0600

    git ignore

--- rust-playground/target/debug/deps/rust_playground-a72398825539b2cb.d
+++
@@ -1,5 +0,0 @@
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-a72398825539b2cb: src/main.rs
-
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-a72398825539b2cb.d: src/main.rs
-
-src/main.rs:
--- rust-playground/target/debug/rust-playground.d
+++
@@ -1,1 +0,0 @@
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/rust-playground: /home/remon/Rusty/Rust-Practice/rust-playground/src/main.rs
