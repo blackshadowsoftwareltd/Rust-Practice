ea5967bf3348d688b0270341d3890723a476ea96
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-12 02:13:02 +0600

    _

--- rust-playground/target/debug/deps/rust_playground-6d50e7c95b84c107.d
+++
@@ -1,8 +0,0 @@
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-6d50e7c95b84c107.rmeta: src/main.rs Cargo.toml
-
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-6d50e7c95b84c107.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.d
+++
@@ -1,5 +0,0 @@
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.rmeta: src/main.rs
-
-/home/remon/Rusty/Rust-Practice/rust-playground/target/debug/deps/rust_playground-86f001739206a4ab.d: src/main.rs
-
-src/main.rs:
