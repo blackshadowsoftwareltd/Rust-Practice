d47223f0044ea39dc0468727b1c1de5e4679d01b
remon <blackshadowsoftwareltd@gmail.com>
2023-03-12 21:07:42 +0600

    tupple example

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,29 +1,10 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let x: i32 = 5;
-    let y: i32 = 10;
-    let z: u32 = 2;
-
-    // Integer addition
-    println!("1 + 2 = {}", 1i32 + 2i32 + x + y); // u8 + i32 not supported
-
-    // Integer subtraction
-    println!("1 - 2 = {}", 1 - 2i32); // u8 - i32 not supported
-
-    // Short-circuiting boolean logic
-    let a = false;
-    let b = true;
-    println!("true AND false is {}", a && b);
-    println!("true OR false is {}", a || b);
-    println!("NOT true is {}", !b);
-
-    println!("1 << 5 is {}", z << 5);
-    println!("0x80 >> 2 is 0 x {:x}", 0x80u32 >> z);
-
-    // Use underscores to improve readability!
-    println!("One million is written as {}", 1_000_000u32);
-
-    println!("{}", y / x);
-    println!("{}", (y * y + 3) % x)
+    let pair: (i64, bool) = (10, true);
+    println!("tupple practice {:?}", tupple_example(pair));
+}
+fn tupple_example(pair: (i64, bool)) -> (i64, i64, bool) {
+    let (int_param, bool_param) = pair;
+    (int_param, int_param * int_param, !bool_param)
 }
