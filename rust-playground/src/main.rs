a6e0c45fabe2181f9252ac76f587bc0e5ff0e78a
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-13 21:42:43 +0600

    Tuple

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,10 +1,23 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let pair: (i64, bool) = (10, true);
-    println!("tupple practice {:?}", tupple_example(pair));
+    let long_tuple = (
+        1u8, 2u16, 3u32, 3u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 3u32), (3u64, -1i8), -2i16);
+    println!("tuple of tuples : {:?}", tuple_of_tuples);
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+
+    // Tuples can be destructured to create bindings.
+    let tuple = (1, "Hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("{:?} {:?} {:?} {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix.0);
 }
-fn tupple_example(pair: (i64, bool)) -> (i64, i64, bool) {
-    let (int_param, bool_param) = pair;
-    (int_param, int_param * int_param, !bool_param)
-}
+struct Matrix(f32, f32, f32, f32);
