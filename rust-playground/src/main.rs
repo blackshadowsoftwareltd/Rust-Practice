8b8b9b467a8b9c8de9a81959a6a50b70e0bbdc06
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-14 23:51:42 +0600

    taking user input (String & int)

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,36 +1,43 @@
 #![deny(clippy::all)]
-
-use std::mem;
-
+use std::io;
 fn main() {
-    //* Array. signature [T; length].
-    let arr: [u8; 5] = [1, 2, 3, 4, 5]; // [data type;length]
+    let mut input: String = String::new();
+    let mut name: String = String::new();
+    let mut age: u8 = 0;
 
-    // All elements can be initialized to the same value.
-    let arr2: [u8; 50] = [1; 50]; // [1,1,1,1,1,1,1,1,1,.....]
-    println!("First element of the array: {:?}", arr);
-    println!("Second element of the array: {:?}", arr2);
-    println!(
-        "Length First array : {} & Second array : {}",
-        arr.len(),
-        arr2.len()
-    );
-    // mem::size_of <T> () > Returns the size of the type in bytes.
-    println!(
-        "First Array occupies {} bytes & Second Array occupies {} bytes",
-        arr.len() * mem::size_of::<u8>(),
-        arr2.len() * mem::size_of::<u8>(),
-    );
-    // mem::size_of_val > Returns the size of the pointed-to value in bytes.
-    println!(
-        "First Array occupies {} bytes & Second Array occupies {} bytes",
-        mem::size_of_val(&arr),
-        mem::size_of_val(&arr2),
-    );
-    //* Slice. signature &[T]
-    let _slice: &[u8];
+    dbg!(name, age); //? dbg! means debug.
+    age = 0;
+    dbg!(age);
+    println!("Enter your Name : ");
 
-    // Arrays can be automatically borrowed as slices.
-    let slice_from_arr: &[u8] = &arr; // memory to memory copy
-    println!("Slice from Array: {:?}", slice_from_arr);
+    //? take user input as Sting using io::stdin().read_line(&mut input)
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    //? remove white space from input using and convert it to string then store it in name variable
+    name = input.trim().to_string();
+
+    //  ! clear input variable !! otherwise it will be make problem in next input
+    input = String::new();
+
+    println!("Enter your Age : ");
+
+    //? take user input as Sting using io::stdin().read_line(&mut input)
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    //? match is kind of switch case in rust
+    //? input.trim().parse::<u8>() convert input to u8
+    match input.trim().parse::<u8>() {
+        Ok(a) => age = a, //? if input is valid then store it in age variable
+        Err(e) => {
+            //? if input is invalid then print error and return
+            println!("Invalid input,Enter number 0 to 255 {}", e);
+            return;
+        }
+    };
+
+    println!("You Name is : {}, And your age is {}", name, age);
 }
