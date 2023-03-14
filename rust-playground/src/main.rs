8dc410386db8a7c79eb399e20898ddb55316bf43
remon <blackshadowsoftwareltd@gmail.com>
2023-03-14 07:31:43 +0600

    declaration of Array & Slice

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,23 +1,36 @@
 #![deny(clippy::all)]
 
+use std::mem;
+
 fn main() {
-    let long_tuple = (
-        1u8, 2u16, 3u32, 3u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    //* Array. signature [T; length].
+    let arr: [u8; 5] = [1, 2, 3, 4, 5]; // [data type;length]
+
+    // All elements can be initialized to the same value.
+    let arr2: [u8; 50] = [1; 50]; // [1,1,1,1,1,1,1,1,1,.....]
+    println!("First element of the array: {:?}", arr);
+    println!("Second element of the array: {:?}", arr2);
+    println!(
+        "Length First array : {} & Second array : {}",
+        arr.len(),
+        arr2.len()
     );
-    println!("long tuple first value: {}", long_tuple.0);
-    println!("Long tuple second value: {}", long_tuple.1);
+    // mem::size_of <T> () > Returns the size of the type in bytes.
+    println!(
+        "First Array occupies {} bytes & Second Array occupies {} bytes",
+        arr.len() * mem::size_of::<u8>(),
+        arr2.len() * mem::size_of::<u8>(),
+    );
+    // mem::size_of_val > Returns the size of the pointed-to value in bytes.
+    println!(
+        "First Array occupies {} bytes & Second Array occupies {} bytes",
+        mem::size_of_val(&arr),
+        mem::size_of_val(&arr2),
+    );
+    //* Slice. signature &[T]
+    let _slice: &[u8];
 
-    let tuple_of_tuples = ((1u8, 2u16, 3u32), (3u64, -1i8), -2i16);
-    println!("tuple of tuples : {:?}", tuple_of_tuples);
-    println!("One element tuple: {:?}", (5u32,));
-    println!("Just an integer: {:?}", (5u32));
-
-    // Tuples can be destructured to create bindings.
-    let tuple = (1, "Hello", 4.5, true);
-    let (a, b, c, d) = tuple;
-    println!("{:?} {:?} {:?} {:?}", a, b, c, d);
-
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix.0);
+    // Arrays can be automatically borrowed as slices.
+    let slice_from_arr: &[u8] = &arr; // memory to memory copy
+    println!("Slice from Array: {:?}", slice_from_arr);
 }
-struct Matrix(f32, f32, f32, f32);
