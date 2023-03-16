5c437e96a07920b9d4b88d7971b29b8a1186ba56
remon <blackshadowsoftwareltd@gmail.com>
2023-03-16 08:18:07 +0600

    Shadowing

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,43 +1,21 @@
 #![deny(clippy::all)]
-use std::io;
+
 fn main() {
-    let mut input: String = String::new();
-    let mut name: String = String::new();
-    let mut age: u8 = 0;
+    let x = 5;
 
-    dbg!(name, age); //? dbg! means debug.
-    age = 0;
-    dbg!(age);
-    println!("Enter your Name : ");
+    println!("Initial value of x is: {x}");
 
-    //? take user input as Sting using io::stdin().read_line(&mut input)
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
+    let x = x + 1;
+    println!("New value of x is: {x}");
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
 
-    //? remove white space from input using and convert it to string then store it in name variable
-    name = input.trim().to_string();
-
-    //  ! clear input variable !! otherwise it will be make problem in next input
-    input = String::new();
-
-    println!("Enter your Age : ");
-
-    //? take user input as Sting using io::stdin().read_line(&mut input)
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
-
-    //? match is kind of switch case in rust
-    //? input.trim().parse::<u8>() convert input to u8
-    match input.trim().parse::<u8>() {
-        Ok(a) => age = a, //? if input is valid then store it in age variable
-        Err(e) => {
-            //? if input is invalid then print error and return
-            println!("Invalid input,Enter number 0 to 255 {}", e);
-            return;
-        }
-    };
-
-    println!("You Name is : {}, And your age is {}", name, age);
+    println!("The value of x is: {x}");
 }
+//? printed :
+// Initial value of x is: 5
+// New value of x is: 6
+// The value of x in the inner scope is: 12
+// The value of x is: 6
