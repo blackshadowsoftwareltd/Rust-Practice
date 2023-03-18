cf5563a61b10986d258a264460703f420eec42a9
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-18 23:06:13 +0600

    type convert & concat

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,21 +1,25 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let x = 5;
+    let x_str: &str = "10";
+    let x_i32: i32 = x_str.parse::<i32>().unwrap();
+    println!("x_str {} to x_i32 = {}", x_str, x_i32);
 
-    println!("Initial value of x is: {x}");
+    let x_float: f32 = x_str.parse::<f32>().unwrap();
+    println!("x_str {} to x_float = {}", x_str, x_float);
 
-    let x = x + 1;
-    println!("New value of x is: {x}");
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let int_to_str: &str = &x_i32.to_string();
+    println!("x_i32 {} to int_float_to_str = {}", x_i32, int_to_str);
 
-    println!("The value of x is: {x}");
+    let float_to_str: &str = &x_float.to_string();
+    println!("x_float {} to float_to_str = {}", x_float, float_to_str);
+
+    let concat_string: String = format!(
+        "{}, {}, {}, {}, {}",
+        x_str, x_i32, x_float, int_to_str, float_to_str
+    );
+    println!("concat_String = {}", concat_string);
+
+    let concat_str: &str = concat_string.as_str(); // String and str is not same type. So, need to convert String to str.
+    println!("concat_str = {}", concat_str);
 }
-//? printed :
-// Initial value of x is: 5
-// New value of x is: 6
-// The value of x in the inner scope is: 12
-// The value of x is: 6
