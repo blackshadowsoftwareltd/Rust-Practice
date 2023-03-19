9b6028c0cd92a1b1b5b17be0e0415263fabebc09
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-19 23:17:39 +0600

    type convert in (int to uint to int)

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,25 +1,46 @@
 #![deny(clippy::all)]
-
+use std::convert::TryInto;
 fn main() {
-    let x_str: &str = "10";
-    let x_i32: i32 = x_str.parse::<i32>().unwrap();
-    println!("x_str {} to x_i32 = {}", x_str, x_i32);
+    //? i8 to i128
+    let i_8: i8 = 5;
+    let i_16: i16 = i16::from(i_8);
+    let i_32: i32 = i32::from(i_16);
+    let i_64: i64 = i64::from(i_32);
+    let i_128: i128 = i128::from(i_64);
+    println!("i 128 : {}", i_128);
 
-    let x_float: f32 = x_str.parse::<f32>().unwrap();
-    println!("x_str {} to x_float = {}", x_str, x_float);
+    //? i128 to i8
+    let i_8: Result<i8, _> = i_128.try_into();
+    match i_8 {
+        Ok(num_i8) => println!("Converted to i8 {}", num_i8),
+        Err(_) => println!("Error : The value is outside of i8"),
+    }
 
-    let int_to_str: &str = &x_i32.to_string();
-    println!("x_i32 {} to int_float_to_str = {}", x_i32, int_to_str);
+    //? u8 to u128
+    let u_8: u8 = 5;
+    let u_16: u16 = u16::from(u_8);
+    let u_32: u32 = u32::from(u_16);
+    let u_64: u64 = u_32 as u64; // as also support
+    let u_128: u128 = u128::from(u_64);
+    println!("u 128 : {}", u_128);
 
-    let float_to_str: &str = &x_float.to_string();
-    println!("x_float {} to float_to_str = {}", x_float, float_to_str);
+    //? u128 to u8
+    let u_8: Result<u8, _> = u_128.try_into();
+    match u_8 {
+        Ok(num_u8) => println!("u128 Converte to u8 {}", num_u8),
+        Err(_) => println!("Error : The value is outside of u8"),
+    }
 
-    let concat_string: String = format!(
-        "{}, {}, {}, {}, {}",
-        x_str, x_i32, x_float, int_to_str, float_to_str
-    );
-    println!("concat_String = {}", concat_string);
-
-    let concat_str: &str = concat_string.as_str(); // String and str is not same type. So, need to convert String to str.
-    println!("concat_str = {}", concat_str);
+    //? i16 to u16
+    let u_16: Result<u16, _> = i_16.try_into();
+    match u_16 {
+        Ok(num_u16) => println!("ui16 Converte to u16 {}", num_u16),
+        Err(_) => println!("Error : The value is outside of u16"),
+    }
+    //? u32 to i32
+    let i_32: Result<i32, _> = u_32.try_into();
+    match i_32 {
+        Ok(num_i32) => println!("u32 Converte to u8 {}", num_i32),
+        Err(_) => println!("Error : The value is outside of i32"),
+    }
 }
