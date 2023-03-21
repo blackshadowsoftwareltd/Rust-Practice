d5fcec19cc5218d1e38daba34f848ac38d495260
remon <blackshadowsoftwareltd@gmail.com>
2023-03-21 22:51:54 +0600

    slice

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,15 +1,9 @@
 #![deny(clippy::all)]
+
 fn main() {
-    let i_32: i32 = 15;
-    let f_32: f32 = i_32 as f32;
-    let f_64: f64 = i_32 as f64;
-    println!("{} {}", f_32, f_64);
-
-    let i_32: i32 = f_32 as i32;
-    let i_64: i32 = f_32 as i32;
-    println!("{} {}", i_32, i_64);
-
-    let f_32: f64 = i_64 as f64;
-    let i_32: i32 = f_64 as i32;
-    println!("{} {}", f_32, i_32);
+    let mut arr = [1, 2, 3, 4, 5];
+    let slice = &mut arr;
+    slice[0] = 10;
+    slice[1] = 20;
+    println!("{:?}", arr);
 }
