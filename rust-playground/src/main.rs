b2f5087ab3d123a2f9c364964b6a15797280fed6
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-25 22:54:32 +0600

    list

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,9 +1,48 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut arr = [1, 2, 3, 4, 5];
-    let slice = &mut arr;
-    slice[0] = 10;
-    slice[1] = 20;
-    println!("{:?}", arr);
+    let mut i = 0;
+
+    //?  While loop
+    while i < 10 {
+        println!("{}", i);
+        i += 1;
+    }
+
+    let mut list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //? For In Loop
+    for e in list {
+        print!("y : {}, ", e);
+    }
+    println!();
+
+    for j in 0..10 {
+        print!("j : {}, ", j);
+    }
+
+    println!();
+
+    list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //? Removes the last element from a vector and returns it, or [None] if it is empty
+    while let Some(y) = list.pop() {
+        print!("y : {}, ", y);
+    }
+
+    //? In a situation where you would like to break or continue for one of the outer loops, you can use labels to specify which loop the break or continue statement applies to.
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 {
+                continue 'outer;
+            } // Continues the loop over `x`.
+            if y % 2 == 0 {
+                continue 'inner;
+            } // Continues the loop over `y`.
+            println!("x: {}, y: {}", x, y);
+        }
+    }
+
+    //?Always alive loop
+    // loop {
+    //     println!("always alive loop");
+    // }
 }
