67e4f6a35c0881c2e1caef7c67d6a02e2a0047b1
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-25 23:45:31 +0600

    list with index & value

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,48 +1,13 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut i = 0;
+    let list = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    //?  While loop
-    while i < 10 {
-        println!("{}", i);
-        i += 1;
+    //? In Rust, the iter() method creates an iterator over the elements of a collection.
+    //? The enumerate() method is then called on the iterator to create a new iterator that yields a tuple of each element of the original iterator and its index.
+    //? So, when you write list.iter().enumerate(), you are creating a new iterator that yields a tuple of each element of the list array and its index. The resulting iterator is then used
+
+    for (i, v) in list.iter().enumerate() {
+        print!("i : {} v : {}, ", i, v);
     }
-
-    let mut list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    //? For In Loop
-    for e in list {
-        print!("y : {}, ", e);
-    }
-    println!();
-
-    for j in 0..10 {
-        print!("j : {}, ", j);
-    }
-
-    println!();
-
-    list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    //? Removes the last element from a vector and returns it, or [None] if it is empty
-    while let Some(y) = list.pop() {
-        print!("y : {}, ", y);
-    }
-
-    //? In a situation where you would like to break or continue for one of the outer loops, you can use labels to specify which loop the break or continue statement applies to.
-    'outer: for x in 0..10 {
-        'inner: for y in 0..10 {
-            if x % 2 == 0 {
-                continue 'outer;
-            } // Continues the loop over `x`.
-            if y % 2 == 0 {
-                continue 'inner;
-            } // Continues the loop over `y`.
-            println!("x: {}, y: {}", x, y);
-        }
-    }
-
-    //?Always alive loop
-    // loop {
-    //     println!("always alive loop");
-    // }
 }
