ceeb338ecaf89d1bf3ff00c0633f4d82e1c1544d
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-26 18:01:28 +0600

    return in loop

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,19 +1,13 @@
 #![deny(clippy::all)]
 
 fn main() {
-    print_me(23, "Remon");
+    let mut count = 0;
 
-    let (age, name) = get_me();
-    println!("My name is : {} and I am {} years old.", name, age);
-}
-
-fn print_me(x: u32, s: &str) {
-    println!("My name is : {} and I am {} years old.", s, x);
-}
-
-fn get_me() -> (u32, String) {
-    let first_name = "Remon";
-    let last_name = "Ahammad";
-    let full_name = format!("{} {}", first_name, last_name);
-    (23, full_name)
+    let rusult = loop {
+        count += 1;
+        if count == 10 {
+            break count;
+        }
+    };
+    println!("The Result is : {}", rusult);
 }
