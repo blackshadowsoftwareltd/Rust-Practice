2de2717792ffa64634ba75f0224166eacce31f64
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-26 17:41:34 +0600

    function

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,13 +1,16 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let list = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    print_me(23, "Remon");
 
-    //? In Rust, the iter() method creates an iterator over the elements of a collection.
-    //? The enumerate() method is then called on the iterator to create a new iterator that yields a tuple of each element of the original iterator and its index.
-    //? So, when you write list.iter().enumerate(), you are creating a new iterator that yields a tuple of each element of the list array and its index. The resulting iterator is then used
+    let (age, name) = get_me();
+    println!("My name is : {} and I am {} years old.", name, age);
+}
 
-    for (i, v) in list.iter().enumerate() {
-        print!("i : {} v : {}, ", i, v);
-    }
+fn print_me(x: u32, s: &str) {
+    println!("My name is : {} and I am {} years old.", s, x);
+}
+
+fn get_me() -> (u32, &'static str) {
+    (23, "Remon Ahammad")
 }
