3ff7e852f6bb10eceeddcc1e03e3cc8aad3ac90e
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-31 23:27:57 +0600

    take ownership & give back

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,21 +1,11 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let s = String::from("Hello");
-    println!("1 {}", s);
-    print_string(s); // ! method took the ownership of the s variable.
-
-    // ! println!("3 {}", s); there is an error.
-    // !caouse ownership is changed to method. to fix this issue =>
-
-    let s = String::from("Hello World");
-    print_string(s.clone()); // we can pass a copy of the variable or
-    print_string_by_reference(&s); // or we can pass the reference to the variable like that
-    println!("-- 1 : {}", s);
+    let mut name = String::from("Remon");
+    println!("{}", name);
+    name = take_ownership_and_give_back(name);
+    println!("{}", name);
 }
-fn print_string(s: String) {
-    println!("2 {}", s);
-}
-fn print_string_by_reference(s: &String) {
-    println!("2 {}", s);
+fn take_ownership_and_give_back(n: String) -> String {
+    n + "Ahammad"
 }
