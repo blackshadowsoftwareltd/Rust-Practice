58014b0143586fd7b5edb2a33d360055a695a2d4
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-31 15:53:45 +0600

    ownership example

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,13 +1,21 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut count = 0;
+    let s = String::from("Hello");
+    println!("1 {}", s);
+    print_string(s); // ! method took the ownership of the s variable.
 
-    let rusult = loop {
-        count += 1;
-        if count == 10 {
-            break count;
-        }
-    };
-    println!("The Result is : {}", rusult);
+    // ! println!("3 {}", s); there is an error.
+    // !caouse ownership is changed to method. to fix this issue =>
+
+    let s = String::from("Hello World");
+    print_string(s.clone()); // we can pass a copy of the variable or
+    print_string_by_reference(&s); // or we can pass the reference to the variable like that
+    println!("-- 1 : {}", s);
+}
+fn print_string(s: String) {
+    println!("2 {}", s);
+}
+fn print_string_by_reference(s: &String) {
+    println!("2 {}", s);
 }
