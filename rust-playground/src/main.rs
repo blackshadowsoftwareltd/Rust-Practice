04faa685a1a0bf0091b05029959c65576697875d
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-07 22:50:43 +0600

    use reference to avoide Ownership

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,21 +1,14 @@
 #![deny(clippy::all)]
 
 fn main() {
-    // ? immutable
-    let s = String::from("String");
-    let x = &s;
-    let y = &s;
-    println!("{}, {}, {}", s, x, y);
-
-    // ? mutable
-    let mut z = String::from("value");
-    let x = &z;
-    let y = &z;
-    println!("{}, {}, {}", z, x, y);
-
-    let xy = &mut z;
-    println!("{}", xy);
-
-    // ! println!("{}, {}, {}", z, x, y); we can not use x, y variable any more.
-    // ! cause z ownershit took the xy variable.
+    let name = "Remon".to_string();
+    println!("Value : {}", name);
+    greet(&name);
+    println!("Value : {}", name);
+    // ! Ownership does not work there cause used "&". 
+    // ! "&" made the reference to the name variable. 
+    // ! That means a copy of the variable is passed to the method, not Ownership.
+}
+fn greet(name: &String) {
+    println!("Name {}", name);
 }
