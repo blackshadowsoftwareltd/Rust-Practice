877213ba021dd6438251943e0eaca425c5860c7b
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-10 03:50:42 +0600

    limitation of mut reference

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -3,17 +3,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    //? cannot borrow `s` as mutable more than once at a time
-    // ! let r1 = &mut s;
-    // ! let r2 = &mut s;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; //! BIG PROBLEM
 
-    //? so we can create bloc of code for each code
-    {
-        let r1 = &mut s;
-        println!("{}", r1)
-    }
-    {
-        let r2 = &mut s;
-        println!("{}", r2)
-    }
+    println!("{}, {}, and {}", r1, r2, r3);
 }
