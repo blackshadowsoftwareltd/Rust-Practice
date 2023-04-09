f38eeafd5d02db3cf08002891b69173e5c1bdf89
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-10 03:49:14 +0600

    limition of mut reference

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,30 +1,19 @@
 #![deny(clippy::all)]
 
 fn main() {
-    // ! store data. kind of ternary operator
-    let name = if 10 == 100 {
-        String::from("Remon")
-    } else {
-        String::from("Ahammad")
-    };
-    println!("The name is {}", name);
+    let mut s = String::from("hello");
 
-    // ! stor data when loop is iterating
-    let list = [1, 2, 3, 4, 5];
-    let mut count = 0;
-    let result = loop {
-        if list.len() == count {
-            break 0;
-        }
-        if 10 == list[count] {
-            break list[count];
-        }
-        count += 1;
-    };
-    let found = if result == 0 {
-        "Not Found".to_string()
-    } else {
-        "Found".to_string()
-    };
-    println!("result : {}", found);
+    //? cannot borrow `s` as mutable more than once at a time
+    // ! let r1 = &mut s;
+    // ! let r2 = &mut s;
+
+    //? so we can create bloc of code for each code
+    {
+        let r1 = &s;
+        println!("{}", r1)
+    }
+    {
+        let r2 = &mut s;
+        println!("{}", r2)
+    }
 }
