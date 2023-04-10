0e47b44e8fa4b7435d6bac6ab4621cc159703896
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-11 01:29:12 +0600

    inline function

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,11 +1,25 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut s = String::from("hello");
+    // ? Inline function. let variable=|parameter type| return something
+    // *  let say_hello:|String| -> String = |name: String| format!("Hello, {}", name);
+    let say_hello = |name: String| format!("Hello, {}", name);
+    println!("{}", say_hello("Remon".to_string()));
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    let r3 = &mut s; //! BIG PROBLEM
+    // *  let sum:|u8, u8| -> u8 = |x: u8, y: u8| x + y;
+    let sum = |x: u8, y: u8| x + y;
+    println!("x + y = {}", sum(2, 4));
 
-    println!("{}, {}, and {}", r1, r2, r3);
+    // * let discount_calculate: |f64,f64| -> (f64, f64, f64, f64) = |price: f64, parcent: f64| { return }
+    let discount_calculate = |price: f64, parcent: f64| {
+        let d = (price * parcent) / 100.0;
+        (price, parcent, d, price - d)
+    };
+
+    let price = discount_calculate(199.0, 10.0);
+
+    println!(
+        "Product price : {} tk, {} % discount, discount : {} tk and subtotal price is : {} tk",
+        price.0, price.1, price.2, price.3
+    );
 }
