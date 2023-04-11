218b25440c93acfe9b000c35b5bb9604bfa32916
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-12 02:12:56 +0600

    reset. call by Point::function(); non associated function

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -3,9 +3,15 @@
 fn main() {
     let point = Point(0.0, 1.0, 2.0);
     point.discribe();
+
     let mut new_point = point.twice();
     new_point.discribe();
+
     new_point.twice_muted();
+    new_point.discribe();
+
+    // ? reset call by Point::reset();
+    new_point = Point::reset(); // ? non associated function
     new_point.discribe();
 }
 struct Point(f32, f32, f32); // ? tuple.
@@ -24,8 +30,14 @@ impl Point {
         self.1 *= 2.0;
         self.2 *= 2.0;
     }
+
+    // ? non associated function
+    fn reset() -> Point {
+        Point(0.0, 0.0, 0.0)
+    }
 }
 // * print ->
 // * Point 0, 1, 2
 // * Point 0, 2, 4
 // * Point 0, 4, 8
+// * Point 0, 0, 0
