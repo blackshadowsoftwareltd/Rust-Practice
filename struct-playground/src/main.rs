72571ad8b6ec72510b21083fb0d8b19a594107df
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-12 01:28:43 +0600

    create instance by another instance properties. called update syntax

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -1,14 +1,36 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let person: Person = Person {
-        name: "Remon".to_string(),
-        age: 23,
+    let person1 = create_person("Remon".to_string(), 23);
+    println!(
+        "Person 1 name {} {}, and age {}",
+        person1.first_name, person1.last_name, person1.age
+    );
+    let person2: Person = Person {
+        first_name: "MR".to_string(),
+        ..person1 // ? updat syntax. It will update all properties of the struct which are not assigned before.
     };
-    println!("My name {}, I am {} years old", person.name, person.age)
+    println!(
+        "Person 2 name {} {}, and age {}",
+        person2.first_name, person2.last_name, person2.age
+    );
+}
+
+fn create_person(first_name: String, age: u8) -> Person {
+    let last_name = "Ahammad".to_string();
+    Person {
+        first_name,
+        last_name,
+        age,
+    }
 }
 
 struct Person {
-    name: String,
+    first_name: String,
+    last_name: String,
     age: u8,
 }
+
+// * print ->
+// * Person 1 name Remon Ahammad, and age 23
+// * Person 2 name MR Ahammad, and age 23
