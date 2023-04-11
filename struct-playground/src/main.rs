52fb9a0dca1b6cf67ddf28a6f605da1a72dea51e
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-12 01:35:31 +0600

    tuple

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -1,36 +1,7 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let person1 = create_person("Remon".to_string(), 23);
-    println!(
-        "Person 1 name {} {}, and age {}",
-        person1.first_name, person1.last_name, person1.age
-    );
-    let person2: Person = Person {
-        first_name: "MR".to_string(),
-        ..person1 // ? updat syntax. It will update all properties of the struct which are not assigned before.
-    };
-    println!(
-        "Person 2 name {} {}, and age {}",
-        person2.first_name, person2.last_name, person2.age
-    );
+    let point = Point(0.0, 1.0, 2.0);
+    println!("Point {}, {}, {}", point.0, point.1, point.2);
 }
-
-fn create_person(first_name: String, age: u8) -> Person {
-    let last_name = "Ahammad".to_string();
-    Person {
-        first_name,
-        last_name,
-        age,
-    }
-}
-
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8,
-}
-
-// * print ->
-// * Person 1 name Remon Ahammad, and age 23
-// * Person 2 name MR Ahammad, and age 23
+struct Point(f32, f32, f32); // ? tuple.
