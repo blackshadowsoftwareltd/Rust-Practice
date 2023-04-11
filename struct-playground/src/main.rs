f9329e67ab030c0cd04937e6c70e9be66b3665d0
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-12 02:08:09 +0600

    mutable example

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -3,7 +3,9 @@
 fn main() {
     let point = Point(0.0, 1.0, 2.0);
     point.discribe();
-    let new_point = point.twice();
+    let mut new_point = point.twice();
+    new_point.discribe();
+    new_point.twice_muted();
     new_point.discribe();
 }
 struct Point(f32, f32, f32); // ? tuple.
@@ -16,4 +18,14 @@ impl Point {
     fn twice(&self) -> Point {
         Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
     }
+
+    fn twice_muted(&mut self) {
+        self.0 *= 2.0;
+        self.1 *= 2.0;
+        self.2 *= 2.0;
+    }
 }
+// * print ->
+// * Point 0, 1, 2
+// * Point 0, 2, 4
+// * Point 0, 4, 8
