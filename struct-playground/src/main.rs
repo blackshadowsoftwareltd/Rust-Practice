d828ba58cbc288229108f97efb32011d440dd052
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-04-12 01:48:46 +0600

    return the struct in function

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -2,13 +2,18 @@
 
 fn main() {
     let point = Point(0.0, 1.0, 2.0);
-    point.discribe(); // ? print Point tuple another way by impl
+    point.discribe();
+    let new_point = point.twice();
+    new_point.discribe();
 }
 struct Point(f32, f32, f32); // ? tuple.
 
-// ? imple self
 impl Point {
     fn discribe(&self) {
         println!("Point {}, {}, {}", self.0, self.1, self.2);
+    }
+
+    fn twice(&self) -> Point {
+        Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
     }
 }
