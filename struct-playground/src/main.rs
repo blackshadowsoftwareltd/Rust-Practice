7035659b43e8169bf4ac12b328b1f7f227802ea8
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-19 23:17:42 +0600

    tuple in enum

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -1,37 +1,37 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let circle = Shapes::Circle {
-        radius: 80,
-        center: (10, 20),
-    };
-    let rect = Shapes::Rectangle {
-        width: 50,
-        height: 40,
-    };
-    match circle {
-        Shapes::Circle { radius, center } => println!(
-            "Circle Radius {:?}, Center {:?} & {:?}",
-            radius, center.0, center.1
-        ),
-        Shapes::Rectangle { width, height } => {
-            println!("Ractangle width{:?}, height {:?}", width, height)
-        }
+    let circle = Shapes::Circle(10.0, 20.0);
+    if let Shapes::Circle(x, y) = circle {
+        println!("Circle {:?} & {:?}", x, y);
     }
-
+    let rect = Shapes::Rectangle(
+        20.0,
+        40.0,
+        Size {
+            width: 15.0,
+            height: 25.0,
+        },
+    );
+    if let Shapes::Rectangle(x, y, Size { width, height }) = rect {
+        println!("Rectangle {:?}, {:?} Size {:?} & {:?}", x, y, width, height);
+    }
+    println!("match ===================================================================");
     match rect {
-        Shapes::Rectangle { width, height } => {
-            println!("Ractangle width{:?}, height {:?}", width, height)
+        Shapes::Circle(x, y) => println!("Circle {:?} & {:?}", x, y),
+        Shapes::Rectangle(x, y, Size { width, height }) => {
+            println!("Rectangle {:?}, {:?} Size {:?} & {:?}", x, y, width, height)
         }
-        Shapes::Circle { radius, center } => println!(
-            "Circle Radius {:?}, Center {:?} & {:?}",
-            radius, center.0, center.1
-        ),
     }
 }
 
-#[derive(Debug)] //? to print enum directly like println!("{:?}",Animals::Cat);S
+#[derive(Debug)] //? to print enum directly like println!("{:?}",Animals::Cat);
 enum Shapes {
-    Circle { radius: i64, center: (i64, i64) },
-    Rectangle { width: i64, height: i64 },
+    Circle(f32, f32),
+    Rectangle(f32, f32, Size),
+}
+#[derive(Debug)] //? to print enum property directly.
+struct Size {
+    width: f32,
+    height: f32,
 }
