cd370e3038540b5ea3564d7f0746da0708fcb624
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-19 22:54:25 +0600

    match in struct type enums

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -5,23 +5,33 @@ fn main() {
         radius: 80,
         center: (10, 20),
     };
-    if let Shapes::Circle { radius, center } = circle {
-        println!(
-            "Circle Radius {:?}, Center {:?} & {:?}",
-            radius, center.0, center.1
-        );
-    }
-    let rect = Shapes::Rectanble {
+    let rect = Shapes::Rectangle {
         width: 50,
         height: 40,
     };
-    if let Shapes::Rectanble { width, height } = rect {
-        println!("Ractangle width{:?}, height {:?}", width, height);
-    };
+    match circle {
+        Shapes::Circle { radius, center } => println!(
+            "Circle Radius {:?}, Center {:?} & {:?}",
+            radius, center.0, center.1
+        ),
+        Shapes::Rectangle { width, height } => {
+            println!("Ractangle width{:?}, height {:?}", width, height)
+        }
+    }
+
+    match rect {
+        Shapes::Rectangle { width, height } => {
+            println!("Ractangle width{:?}, height {:?}", width, height)
+        }
+        Shapes::Circle { radius, center } => println!(
+            "Circle Radius {:?}, Center {:?} & {:?}",
+            radius, center.0, center.1
+        ),
+    }
 }
 
 #[derive(Debug)] //? to print enum directly like println!("{:?}",Animals::Cat);S
 enum Shapes {
     Circle { radius: i64, center: (i64, i64) },
-    Rectanble { width: i64, height: i64 },
+    Rectangle { width: i64, height: i64 },
 }
