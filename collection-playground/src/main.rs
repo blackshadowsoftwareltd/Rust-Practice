0ddbc2b944571e9a419d01264f90eb6a26dc61a0
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-21 00:00:55 +0600

    Hash Map

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -1,51 +1,45 @@
 #![deny(clippy::all)]
+use std::collections::HashMap;
 fn main() {
-    let mut values = vec![1, 2, 3, 4, 5];
-    println!("{:?}", values); // [1, 2, 3, 4, 5]
+    let mut map: HashMap<String, String> = HashMap::new();
+    map.insert("name".to_string(), "Remon Ahammad".to_string());
+    map.insert("age".to_string(), "23".to_string());
+    println!("map : {:?}", map); // map : {"age": "23", "name": "Remon Ahammad"}
 
-    values.push(6);
-    println!("{:?}", values); // [1, 2, 3, 4, 5, 6]
-
-    values[0] = 7;
-    println!("{:?}", values); // [7, 2, 3, 4, 5, 6]
-
-    values.remove(4);
-    println!("{:?}", values); // [7, 2, 3, 4, 6]
-
-    values.pop();
-    println!("{:?}", values); // [7, 2, 3, 4]
-
-    let mut temp = vec![8, 9, 10];
-    values.extend_from_slice(&temp);
-    println!("values {:?}, temp {:?}", values, temp); // [7, 2, 3, 4, 8, 9, 10]   println!("{:?}", values);
-
-    values.clear();
-    println!("{:?}", values); // []
-
-    values.extend_from_slice(&[12, 13, 14, 15, 16, 17, 18]);
-    println!("{:?}", values); // [12,13,14,15,16,17,18]
-
-    values.append(&mut temp);
-    println!("values {:?}, temp {:?}", values, temp); // values [12, 13, 14, 15,16,17,18, 8, 9, 10], temp []
-
-    if values.contains(&15) {
-        println!("YES")
-    } else {
-        println!("NO")
+    map.remove("age");
+    println!("map : {:?}", map); // map : {"name": "Remon Ahammad"}
+    if map.contains_key("age") {
+        println!("Exist");
     }
 
-    println!("is values are empty? {}", values.is_empty()); // is values are empty? false
+    let name = map.get("name");
+    match name {
+        Some(name) => println!("The value of name is : {}", name),
+        None => println!("the key does not exist"),
+    }
 
-    let filtered = values
-        .into_iter()
-        .filter(|&x| x > 10 && x < 15)
-        .collect::<Vec<_>>();
-    println!("Filtered {:?}", filtered); // Filtered [12, 13, 14]
+    for (k, v) in &map {
+        println!("K : {}, V : {}", k, v);
+    }
 
-    values = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let first_where = values.into_iter().find(|&x| x > 4 && x < 8);
-    match first_where {
-        Some(n) => println!("{:?}", n), // 5
-        None => println!("NONE"),       // if not contain? => NONE
-    };
+    let entry = map.entry("name".to_string());
+    println!("Entry : k : {}", entry.key()); // Entry : k : name
+    match entry {
+        std::collections::hash_map::Entry::Occupied(e) => {
+            // ! Occupied means exist
+            println!("The value of Entry is : {:?}", e.get())
+        }
+        std::collections::hash_map::Entry::Vacant(e) => {
+            // ! Vacant means Not exist
+            println!("The {:?} key does not exist ", e.key())
+        }
+    }
+
+    map.entry(String::from("name"))
+        .or_insert("default".to_string());
+    println!("map : {:?}", map); // map : {"name": "Remon Ahammad"}
+
+    map.entry(String::from("default_key"))
+        .or_insert("default".to_string());
+    println!("map : {:?}", map); // map : {"name": "Remon Ahammad", "default_key": "default"}
 }
