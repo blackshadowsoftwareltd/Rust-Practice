24a26a8f3ec08585522d7aa2a9780ecf32766437
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-22 10:57:33 +0600

    expect. in option

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -2,8 +2,10 @@
 
 fn main() {
     let name: Option<String> = Some("Remon Ahammad".to_string());
-    match name {
-        Some(n) => println!("The name is : {}", n),
-        None => println!("There is no name"),
-    }
+    let unwrapped_name = name.expect("Name not found");
+    println!("the name is : {unwrapped_name}"); // the name is : Remon Ahammad
+
+    let name: Option<String> = None;
+    let unwrapped_name = name.expect("Name is not found");
+    println!("the name is : {unwrapped_name}"); // thread 'main' panicked at 'Name is not found', src/main.rs:9:31
 }
