803fadf8bae18fc109dda981aed9db3670911e15
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-22 10:51:34 +0600

    option. Some

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let name: Option<String> = None;
+    let name: Option<String> = Some("Remon Ahammad".to_string());
     match name {
         Some(n) => println!("The name is : {}", n),
         None => println!("There is no name"),
