a095deaacab3795a11c31753f76f618de593ffeb
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-22 10:46:51 +0600

    optional

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -1,19 +1,9 @@
 #![deny(clippy::all)]
-use std::collections::HashMap;
 
 fn main() {
-    let user1 = Person {
-        name: "Remon Ahammad".to_string(),
-        age: 23,
-    };
-    println!("name : {}, age : {}", user1.name, user1.age);
-
-    let mut map: HashMap<String, Person> = HashMap::new();
-    map.insert("person".to_string(), user1);
-    println!("Map : {:?}", map); // Map : {"person": Person { name: "Remon Ahammad", age: 23 }}
-}
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
+    let name: Option<String> = None;
+    match name {
+        Some(n) => println!("The name is : {}", n),
+        None => println!("There is no name"),
+    }
 }
