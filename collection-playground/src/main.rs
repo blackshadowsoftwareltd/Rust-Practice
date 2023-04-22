df085a99fb9f48c34e7f255ff21dd30b1e93eb91
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-22 11:06:47 +0600

    as mut

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -1,11 +1,18 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let name: Option<String> = Some("Remon Ahammad".to_string());
-    let unwrapped_name = name.expect("Name not found");
-    println!("the name is : {unwrapped_name}"); // the name is : Remon Ahammad
+    let mut age: Option<u8> = Some(23);
+    match age.as_mut() {
+        Some(age) => *age += 10,
+        None => println!("Envalid"),
+    }
+    println!("Age : {}", age.unwrap()); // Age : 33
 
-    let name: Option<String> = None;
-    let unwrapped_name = name.expect("Name is not found");
-    println!("the name is : {unwrapped_name}"); // thread 'main' panicked at 'Name is not found', src/main.rs:9:31
+    let mut age: Option<u8> = None;
+    match age.as_mut() {
+        Some(age) => *age += 10,
+        None => println!("Envalid"),
+    }
+    println!("Age : {}", age.unwrap()); // !Envalid
+                                        // !thread 'main' panicked at 'called `Option::unwrap()` on a `None` value', src/main.rs:16:30
 }
