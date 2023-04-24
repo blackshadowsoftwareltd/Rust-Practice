770cdc3f33e5b6618226404db8137bb09b029671
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-24 10:33:17 +0600

    err handle with fn

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,10 +1,23 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Result : {}", result().is_ok()); // Result : true
-    println!("Result : {}", result().is_err()); // Result : false
+    let full_name = full_name();
+    match full_name {
+        Ok(v) => println!("Full Name : {}", v), // Full Name : Remon Ahammad
+        Err(_) => println!("Error"),            // Error
+    }
 }
 
-fn result() -> Result<String, ()> {
-    Ok("Success".to_string())
+fn first_name() -> Result<String, ()> {
+    Ok("Remon".to_string())
+}
+
+fn last_name() -> Result<String, ()> {
+    Ok("Ahammad".to_string())
+}
+
+fn full_name() -> Result<String, ()> {
+    let first_name = first_name()?;
+    let last_name = last_name()?;
+    Ok(format!("{} {}", first_name, last_name))
 }
