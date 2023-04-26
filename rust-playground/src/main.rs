eed2d4d73c76cc0172841cc0008d2cef3a09cc7a
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-26 23:08:21 +0600

    match with unwrap_or(default:0)

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,23 +1,19 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let full_name = full_name();
-    match full_name {
-        Ok(v) => println!("Full Name : {}", v), // Full Name : Remon Ahammad
-        Err(_) => println!("Error"),            // Error
+    let five = Some(5);
+
+    let six_option = sum_one(five);
+    let six = six_option.unwrap_or(0);
+    println!("{}", six); // 6
+
+    let none_option = sum_one(None);
+    let none = none_option.unwrap_or(0);
+    println!("{}", none) // 0
+}
+fn sum_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(_) => x.map(|v| v + 1),
     }
-}
-
-fn first_name() -> Result<String, ()> {
-    Ok("Remon".to_string())
-}
-
-fn last_name() -> Result<String, ()> {
-    Ok("Ahammad".to_string())
-}
-
-fn full_name() -> Result<String, ()> {
-    let first_name = first_name()?;
-    let last_name = last_name()?;
-    Ok(format!("{} {}", first_name, last_name))
 }
