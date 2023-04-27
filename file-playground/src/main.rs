3f3b9970fa382f140ed5bd75f2540858b419de6e
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-27 23:19:54 +0600

    file create, write, read

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -1,2 +1,31 @@
 #![deny(clippy::all)]
-fn main() {}
+
+use std::fs::{self, File};
+use std::io::prelude::*;
+
+fn main() {
+    // ? create
+    let mut _file = create_file();
+
+    // ? write
+    write_file(&mut _file, "Hello!".to_string());
+
+    // ? read
+    let data = read_file("files/test.txt".to_string());
+    println!("Data : {}", data);
+}
+
+fn create_file() -> File {
+    let mut _file = File::create("files/test.txt").expect("Failed to create file");
+    _file
+}
+
+fn write_file(file: &mut File, message: String) -> &mut File {
+    write!(file, "{}", message).expect("Failed to wirte data");
+    file
+}
+
+fn read_file(path: String) -> String {
+    let contents = fs::read_to_string(path).expect("Unable to read file");
+    contents
+}
