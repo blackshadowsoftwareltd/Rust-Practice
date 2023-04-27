97258cb3196ab1d013ae124ab29565f8902bef5f
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-28 00:04:15 +0600

    optimize. dir create, file create, write, read & delete file

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -2,21 +2,34 @@
 
 use std::fs::{self, File};
 use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
+    let file_path = Path::new("files/test.txt");
+    is_dir_exist(&file_path);
+
     // ? create
-    let mut _file = create_file();
+    let mut _file = create_file(&file_path);
 
     // ? write
     write_file(&mut _file, "Hello!".to_string());
 
     // ? read
-    let data = read_file("files/test.txt".to_string());
+    let data = read_file(&file_path);
     println!("Data : {}", data);
+
+    // ? delete
+    remove_file(&file_path);
 }
 
-fn create_file() -> File {
-    let mut _file = File::create("files/test.txt").expect("Failed to create file");
+fn is_dir_exist(path: &Path) {
+    if !path.exists() {
+        fs::create_dir_all(path.parent().unwrap()).expect("Filed to create Directory");
+    }
+}
+
+fn create_file(path: &Path) -> File {
+    let mut _file = File::create(path.display().to_string()).expect("Failed to create file");
     _file
 }
 
@@ -25,7 +38,14 @@ fn write_file(file: &mut File, message: String) -> &mut File {
     file
 }
 
-fn read_file(path: String) -> String {
+fn read_file(path: &Path) -> String {
     let contents = fs::read_to_string(path).expect("Unable to read file");
     contents
+}
+
+fn remove_file(path: &Path) {
+    match fs::remove_file(path.display().to_string()) {
+        Ok(()) => println!("File deleted successfully!"),
+        Err(error) => println!("Error deleting file: {:?}", error),
+    }
 }
