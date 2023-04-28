960ba13a9874371a593fd641c15111da000748e2
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-04-29 02:35:23 +0600

    image to base64 & base64 to image convert

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -1,25 +1,19 @@
 #![deny(clippy::all)]
 
-use std::fs::{self, File};
-use std::io::prelude::*;
-use std::path::Path;
+use std::{
+    fs::{self, File},
+    io::Read,
+    path::Path,
+};
 
+use base64::{decode, encode};
+use image::load_from_memory;
 fn main() {
-    let file_path = Path::new("files/test.txt");
-    is_dir_exist(&file_path);
-
-    // ? create
-    let mut _file = create_file(&file_path);
-
-    // ? write
-    write_file(&mut _file, "Hello!".to_string());
-
-    // ? read
-    let data = read_file(&file_path);
-    println!("Data : {}", data);
-
-    // ? delete
-    remove_file(&file_path);
+    let image_path = Path::new("files/pxfuel.jpg");
+    is_dir_exist(image_path);
+    let base64_str = convert_image_to_base64(image_path);
+    println!("Base 64 String length : {}", base64_str.len());
+    convert_base64_to_image(base64_str);
 }
 
 fn is_dir_exist(path: &Path) {
@@ -28,24 +22,34 @@ fn is_dir_exist(path: &Path) {
     }
 }
 
-fn create_file(path: &Path) -> File {
-    let mut _file = File::create(path.display().to_string()).expect("Failed to create file");
-    _file
+fn convert_image_to_base64(path: &Path) -> String {
+    let mut image_file: File = File::open(path.display().to_string()).expect("File can't be open");
+    let mut image_data = Vec::new();
+
+    image_file
+        .read_to_end(&mut image_data)
+        .expect("Can't be read to end");
+
+    encode(image_data)
 }
 
-fn write_file(file: &mut File, message: String) -> &mut File {
-    write!(file, "{}", message).expect("Failed to wirte data");
-    file
+fn convert_base64_to_image(base64_str: String) {
+    let image_data: Vec<u8> = decode(base64_str).expect("Failed to decode base64 str");
+    let image: image::DynamicImage =
+        load_from_memory(&image_data).expect("Failed to load from memory");
+
+    let mut file = File::create("files/image.png").expect("Filed to create image");
+    image
+        .write_to(&mut file, image::ImageOutputFormat::Png)
+        .expect("Filed to write to Png");
 }
 
-fn read_file(path: &Path) -> String {
-    let contents = fs::read_to_string(path).expect("Unable to read file");
-    contents
-}
+// let base64_str = "iVBORw0KGgoAAAANSUhEUgAAADIA..."; // replace with your base64 string
+// let data = decode(base64_str)?;
 
-fn remove_file(path: &Path) {
-    match fs::remove_file(path.display().to_string()) {
-        Ok(()) => println!("File deleted successfully!"),
-        Err(error) => println!("Error deleting file: {:?}", error),
-    }
-}
+// // Load the image from the byte array
+// let image = load_from_memory(&data)?;
+
+// // Save the image to a file
+// let mut file = File::create("image.png")?;
+// image.write_to(&mut file, image::ImageOutputFormat::PNG)?;
