a13f4ffca89d1c79d366da3a2297c4d36507829f
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-05-03 08:01:08 +0600

    custom pointer with Deref example

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,11 +1,39 @@
 #![deny(clippy::all)]
+
+use std::ops::Deref;
 fn main() {
-    let mut count: Box<i8> = Box::new(20);
-    println!("count : {}", *count);
+    let count: CustomBox<i8> = CustomBox::new(10);
+    println!("Custom Box count : {}", *count); // ! err: deref error. to avoid this error we need to imple deref struct for CustomBox<T> -> deref()
 
-    let twice = *count * 2;
-    println!("twice : {}", twice);
+    let actual_value: &i8 = count.deref(); // ? getting value without pointer (with reference) using deref()
+    println!("Actual value : {}", actual_value);
 
-    *count *= 2;
-    println!("twice on point of variable : {}", *count);
+    let other: i8 = *(count.deref()); // ? *count == *(count.deref()) similar things
+    println!("Similar to *count == *(count.deref()) : {}", other);
+
+    // ? implicit dereference. rust understand we need i8 (actual count data is CustomBox<i8>)
+    print_value(&count); // ? passing reference of value to a function by using &value
+}
+
+struct CustomBox<T> {
+    value: T,
+}
+
+impl<T> CustomBox<T> {
+    fn new(value: T) -> Self {
+        CustomBox { value }
+    }
+}
+
+// ? Deref will help to count value directly by pointer
+// ? Deref == Dereference
+impl<T> Deref for CustomBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+fn print_value(value: &i8) {
+    println!("print value : {}", value);
 }
