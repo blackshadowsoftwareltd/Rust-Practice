5d2415f1580e8d2df3c724f5bf5020427e12320f
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-06-03 00:58:51 +0600

    simple example of modules

--- crates-package-modules-playground/src/main.rs
+++ crates-package-modules-playground/src/main.rs
@@ -1,3 +1,10 @@
+#![deny(clippy::all)]
+
+use workspace::addition_mod::add;
+use workspace::subtraction_mod::sub;
 fn main() {
-    println!("Hello, world!");
+    let _sum = add(10, 20);
+    println!("{}", _sum);
+    let _sub = sub(20, 10);
+    println!("{}", _sub)
 }
--- crates-package-modules-playground/workspace/src/lib.rs
+++ crates-package-modules-playground/workspace/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod addition_mod {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod subtraction_mod {
+    pub fn sub(a: i32, b: i32) -> i32 {
+        a - b
     }
 }
