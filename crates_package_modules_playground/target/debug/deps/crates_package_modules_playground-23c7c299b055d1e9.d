8f22571d46bf6720ab397a4e9f7c3ba8ba963c5c
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-06-02 23:21:40 +0600

    -

---
+++ crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-23c7c299b055d1e9.d
@@ -0,0 +1,5 @@
+/home/remon/Rusty/Rust-Practice/crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-23c7c299b055d1e9.rmeta: src/main.rs
+
+/home/remon/Rusty/Rust-Practice/crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-23c7c299b055d1e9.d: src/main.rs
+
+src/main.rs:
---
+++ crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-f665237a805deb5f.d
@@ -0,0 +1,5 @@
+/home/remon/Rusty/Rust-Practice/crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-f665237a805deb5f.rmeta: src/main.rs
+
+/home/remon/Rusty/Rust-Practice/crates_package_modules_playground/target/debug/deps/crates_package_modules_playground-f665237a805deb5f.d: src/main.rs
+
+src/main.rs:
