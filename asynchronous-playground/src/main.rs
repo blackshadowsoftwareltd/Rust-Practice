1222a1d3dcd5c4e9e55745cb4ab38947eb29a871
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-06-03 01:49:07 +0600

    async with tokio package

--- asynchronous-playground/src/main.rs
+++ asynchronous-playground/src/main.rs
@@ -1,14 +1,21 @@
 #![deny(clippy::all)]
 
-use futures::executor::block_on;
-fn main() {
-    //? block on come from the package futures.
-    //? block_on help to exicute future.
-    //? otherwise async won't work;
-    let name: String = block_on(get_name());
-    println!("Hello, {}", name)
+use tokio::time::{sleep, Duration};
+#[tokio::main]
+async fn main() {
+    println!("Application Started ---->");
+    let get: String = get_data_from_api().await;
+    println!("Get : {}", get);
+    let post: bool = post_data_to_api(get).await;
+    println!("Is Posted : {}", post);
 }
 
-async fn get_name() -> String {
-    "Remon Ahammad".to_string()
+async fn get_data_from_api() -> String {
+    sleep(Duration::from_secs(2)).await;
+    "Api Response after 2 second later.".to_string()
+}
+
+async fn post_data_to_api(name: String) -> bool {
+    sleep(Duration::from_secs(3)).await;
+    !name.is_empty()
 }
