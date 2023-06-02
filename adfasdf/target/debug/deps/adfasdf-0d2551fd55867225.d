b4117593d8071e15162b0e69e80c7b3b42416ed1
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-06-03 02:12:53 +0600

    -

--- adfasdf/target/debug/deps/adfasdf-0d2551fd55867225.d
+++
@@ -1,5 +0,0 @@
-/home/remon/Rusty/Rust-Practice/adfasdf/target/debug/deps/adfasdf-0d2551fd55867225.rmeta: src/main.rs
-
-/home/remon/Rusty/Rust-Practice/adfasdf/target/debug/deps/adfasdf-0d2551fd55867225.d: src/main.rs
-
-src/main.rs:
--- adfasdf/target/debug/deps/adfasdf-b3713203ce6eff5f.d
+++
@@ -1,5 +0,0 @@
-/home/remon/Rusty/Rust-Practice/adfasdf/target/debug/deps/adfasdf-b3713203ce6eff5f.rmeta: src/main.rs
-
-/home/remon/Rusty/Rust-Practice/adfasdf/target/debug/deps/adfasdf-b3713203ce6eff5f.d: src/main.rs
-
-src/main.rs:
