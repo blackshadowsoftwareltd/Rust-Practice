a1aa29b0f9a0ba231befe24e85393d70c90bd146
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-13 15:11:44 +0600

    struct with mut ref

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -1,36 +1,40 @@
-#![deny(clippy::all)]
-
-use std::fmt;
-
 fn main() {
-    let person = Person::new("Remon Ahammad");
-    println!("{}", person); // First Name : Remon, Last Name : Ahammad
+    let mut rect: Rectangle = Rectangle::new(10, 5);
+    println!("old area : {:?}", rect.area());
+
+    rect.update_width_height_with_ref(20, 10); //? wither ref
+    println!("updated with ref : {:?}", rect.area());
+
+    rect = rect.update_width_height(30, 15); //? without ref
+    println!("updated without ref : {:?}", rect.area());
+
+    rect.inc_width(5);
+    println!("new area : {:?}", rect.area());
 }
 
-impl fmt::Display for Person {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "First Name : {}, Last Name : {}",
-            self.first_name, self.last_name
-        )
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
     }
-}
-
-struct Person {
-    first_name: String,
-    last_name: String,
-}
-
-trait PersonInit {
-    fn new(name: &str) -> Self;
-}
-impl PersonInit for Person {
-    fn new(name: &str) -> Self {
-        let part: Vec<&str> = name.split(' ').collect();
-        Person {
-            first_name: part[0].to_string(),
-            last_name: part[1].to_string(),
-        }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn update_width_height_with_ref(&mut self, w: u32, h: u32) {
+        self.width = w;
+        self.height = h;
+        // ? no need return if self is mut ref
+    }
+    fn update_width_height(mut self, w: u32, h: u32) -> Rectangle {
+        self.width = w;
+        self.height = h;
+        self
+    }
+    fn inc_width(&mut self, delta: u32) {
+        self.width += delta
     }
 }
