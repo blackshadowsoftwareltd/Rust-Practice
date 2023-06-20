0d36824b591f05b43c19451239c1e07030320836
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-20 17:11:39 +0600

    std::iter::Repeat

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,33 +1,9 @@
 #![deny(clippy::all)]
 
-use std::collections::VecDeque;
+use std::iter;
 
 fn main() {
-    let mut _vector: Vec<i32> = Vec::new();
-    for i in 0..5 {
-        _vector.push(i);
-        println!("Vector: {:?}", _vector);
-    }
-    for _i in (0..5).rev() {
-        _vector.pop();
-        println!("Vector: {:?}", _vector);
-    }
-
-    let mut _vec_deque: VecDeque<i32> = VecDeque::new();
-    for i in 0..5 {
-        _vec_deque.push_front(i);
-        println!("VecDeque: {:?}", _vec_deque);
-    }
-    for i in 5..10 {
-        _vec_deque.push_back(i);
-        println!("VecDeque: {:?}", _vec_deque);
-    }
-    for _ in 0..5 {
-        _vec_deque.pop_front();
-        println!("VecDeque: {:?}", _vec_deque);
-    }
-    for _ in (5..10).rev() {
-        _vec_deque.pop_back();
-        println!("VecDeque: {:?}", _vec_deque);
-    }
+    let five_fives_iters: iter::Take<iter::Repeat<i32>> = iter::repeat(5).take(5);
+    let five_fives: Vec<i32> = Vec::from_iter(five_fives_iters);
+    println!("{:?}", five_fives);
 }
