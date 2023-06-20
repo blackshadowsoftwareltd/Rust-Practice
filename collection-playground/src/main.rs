2179fd84bf24c20788de7a59152d838ddd999e14
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-20 15:41:05 +0600

    convert arr to vec using collect

--- collection-playground/src/main.rs
+++ collection-playground/src/main.rs
@@ -1,13 +1,25 @@
 #![deny(clippy::all)]
 
-fn main() {
-    let mut name: Option<String> = None;
-    if name.is_none() {
-        println!("Name is None")
-    }
+use std::collections::VecDeque;
 
-    name = Some("Hello".to_string());
-    if name.is_some() {
-        println!("Name exist")
-    }
+fn main() {
+    let arr: [i32; 3] = [1, 2, 3];
+
+    let double_arr: Vec<i32> = arr.iter().map(|&x| x * 2).collect::<Vec<i32>>();
+    println!("{:?}", double_arr); // [2, 4, 6]
+
+    let double_arr: Vec<i32> = arr.iter().map(|&x| x * 2).collect::<Vec<_>>(); //? collect() only cares about what you’re collecting into, you can still use a partial type hint, _,
+    println!("{:?}", double_arr); // [2, 4, 6]
+
+    let double_arr: VecDeque<i32> = arr.iter().map(|&x| x * 2).collect::<VecDeque<i32>>();
+    println!("{:?}", double_arr); // [2, 4, 6]
+
+    let arr: [char; 5] = ['a', 'b', 'c', 'd', 'e'];
+
+    let chars: String = arr
+        .iter()
+        .map(|&x| x as u8)
+        .map(|x: u8| x as char)
+        .collect::<String>();
+    println!("{:?}", chars); //? "abcde"
 }
