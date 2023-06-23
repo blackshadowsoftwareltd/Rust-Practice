f8e288776d4251afdbb33128392d52f191ac0cde
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-23 23:27:43 +0600

    Pattern Matching

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,37 +1,15 @@
-// #![deny(clippy::all)]
-
-use std::{any::Any, collections::HashMap};
+#![deny(clippy::all)]
 
 fn main() {
-    let mut map: HashMap<String, Box<dyn Any>> = HashMap::new();
-
-    map.insert("full_name".to_string(), Box::new("John Doe".to_string()));
-    map.insert("name".to_string(), Box::new("Alice"));
-    map.insert("age".to_string(), Box::new(30));
-    map.insert("isStudent".to_string(), Box::new(true));
-
-    // Accessing values
-    if let Some(full_name) = map.get("full_name") {
-        if let Some(full_name_str) = full_name.downcast_ref::<String>() {
-            println!("Full Name : {}", full_name_str);
-        }
+    let x: u8 = 5;
+    match x {
+        0 => println!("Zero"),
+        1 | 2 => println!("One or Two"),
+        3..=5 => println!("3 to 5"),
+        _ => println!("Default case"),
     }
 
-    if let Some(name) = map.get("name") {
-        if let Some(name_str) = name.downcast_ref::<&str>() {
-            println!("Name: {}", name_str);
-        }
-    }
-
-    if let Some(age) = map.get("age") {
-        if let Some(age_i32) = age.downcast_ref::<i32>() {
-            println!("Age: {}", age_i32);
-        }
-    }
-
-    if let Some(is_student) = map.get("isStudent") {
-        if let Some(is_student_bool) = is_student.downcast_ref::<bool>() {
-            println!("Is Student: {}", is_student_bool);
-        }
-    }
+    let numbers = (0, 1, 2, 3, 4, 5);
+    let (first, .., last) = numbers;
+    println!("First ? {} & Last ? {}", first, last);
 }
