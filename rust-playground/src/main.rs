08a144a28a20e988f507b120e1257dae324bd968
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-25 13:42:09 +0600

    enum with struct

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,13 +1,38 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let home = IPAddress::V4("12.131.12.13".to_string());
-    let loopback = IPAddress::V6("25.32.24.24".to_string());
+    let home = IPAddress::V4(IPV4 {
+        address: "45.68.78.34".to_string(),
+    });
+    let loopback = IPAddress::V6(IPV6 {
+        address: "25.32.24.24".to_string(),
+    });
     println!("Home: {:?}", home);
     println!("Loopback: {:?}", loopback);
+
+    let home = match home {
+        IPAddress::V4(ipv4) => ipv4.address,
+        IPAddress::V6(ipv6) => ipv6.address,
+    };
+    println!("Home: {}", home);
+
+    let loopback = match loopback {
+        IPAddress::V4(ipv4) => ipv4.address,
+        IPAddress::V6(ipv6) => ipv6.address,
+    };
+    println!("Home: {}", home);
+    println!("Loopback: {}", loopback);
 }
 #[derive(Debug)]
 enum IPAddress {
-    V4(String),
-    V6(String),
+    V4(IPV4),
+    V6(IPV6),
+}
+#[derive(Debug)]
+struct IPV4 {
+    address: String,
+}
+#[derive(Debug)]
+struct IPV6 {
+    address: String,
 }
