025c7fb972468f69e2877ecf208c683f32a02d6d
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-25 17:03:08 +0600

    if let some

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,21 +1,25 @@
 #![deny(clippy::all)]
 
 fn main() {
-    match plus_one(Some(5)) {
-        Some(v) => print!("plus with 5 : {:?}", v),
-        None => println!("None"),
-    };
-    println!("");
-    match plus_one(None) {
-        Some(_) => println!("Some Data"),
-        None => println!("None"),
-    }
-    // println!("plus with None : {:?}", ;
+    let color = Some(Color::Red);
+    color_match(color);
+    let color = Some(Color::Green);
+    color_match(color);
+    let color = Some(Color::Blue);
+    color_match(color);
 }
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(v) => Some(v + 1),
-        None => None,
+fn color_match(color: Option<Color>) {
+    if let Some(Color::Red) = color {
+        println!("The color is Red!");
+    } else if let Some(Color::Green) = color {
+        println!("The color is Green!");
+    } else if let Some(Color::Blue) = color {
+        println!("The color is Blue!");
     }
+}
+enum Color {
+    Red,
+    Green,
+    Blue,
 }
