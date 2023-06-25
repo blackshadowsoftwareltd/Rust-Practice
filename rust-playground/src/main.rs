3306608a5ccb5d4b136fd164fff01c157839713f
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-25 17:06:50 +0600

    if let some with value

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,25 +1,17 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let color = Some(Color::Red);
-    color_match(color);
-    let color = Some(Color::Green);
-    color_match(color);
-    let color = Some(Color::Blue);
-    color_match(color);
+    color_match(Some(0));
+    color_match(Some(1));
+    color_match(Some(2));
 }
 
-fn color_match(color: Option<Color>) {
-    if let Some(Color::Red) = color {
+fn color_match(color: Option<i32>) {
+    if let Some(0) = color {
         println!("The color is Red!");
-    } else if let Some(Color::Green) = color {
+    } else if let Some(1) = color {
         println!("The color is Green!");
-    } else if let Some(Color::Blue) = color {
+    } else if let Some(2) = color {
         println!("The color is Blue!");
     }
-}
-enum Color {
-    Red,
-    Green,
-    Blue,
 }
