f85cc019413fc909f8736d7495f85fc6898e88cc
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-25 13:50:12 +0600

    imple of enum struct

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -10,18 +10,16 @@ fn main() {
     println!("Home: {:?}", home);
     println!("Loopback: {:?}", loopback);
 
-    let home = match home {
-        IPAddress::V4(ipv4) => ipv4.address,
-        IPAddress::V6(ipv6) => ipv6.address,
-    };
-    println!("Home: {}", home);
-
-    let loopback = match loopback {
-        IPAddress::V4(ipv4) => ipv4.address,
-        IPAddress::V6(ipv6) => ipv6.address,
-    };
-    println!("Home: {}", home);
-    println!("Loopback: {}", loopback);
+    println!("Home: {}", home.get_ip());
+    println!("Loopback: {}", loopback.get_ip());
+}
+impl IPAddress {
+    fn get_ip(&self) -> String {
+        match self {
+            IPAddress::V4(ipv4) => ipv4.address.clone(),
+            IPAddress::V6(ipv6) => ipv6.address.clone(),
+        }
+    }
 }
 #[derive(Debug)]
 enum IPAddress {
