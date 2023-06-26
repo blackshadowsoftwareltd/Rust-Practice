87e22e441ffbeba633295d6acba8da8b6efb3480
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-26 14:50:53 +0600

    error handling using Result (Ok,Err)

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -1,31 +1,19 @@
 #![deny(clippy::all)]
-use std::path::Path;
+use std::fs::File;
 
 fn main() {
-    // let bangla_path = Path::new("font/noto_serif_bengali/static/NotoSerifBengali/");
-    // let bangla_name = "NotoSerifBengali";
-    let english_path = Path::new("font/static/");
-    let english_name = "RedHatMono";
+    let result = open_file();
+    if let Ok(_file) = result {
+        println!("File is exist")
+    } else if let Err(message) = result {
+        println!("!!! Error : {}", message);
+    }
+}
 
-    //? Load a font from the file system
-    let font_family = genpdf::fonts::from_files(&english_path, &english_name, None)
-        .expect("Failed to load font family");
-
-    //? Create a document and set the default font family
-    let mut doc = genpdf::Document::new(font_family);
-
-    //? Change the default settings
-    doc.set_title("Demo Document");
-
-    //? Customize the pages
-    let mut decorator = genpdf::SimplePageDecorator::new();
-    decorator.set_margins(10);
-    doc.set_page_decorator(decorator);
-
-    //? Add one or more elements
-    doc.push(genpdf::elements::Paragraph::new("This is a Paragraph"));
-
-    //? Render the document and write it to a file
-    doc.render_to_file("files/output.pdf")
-        .expect("Failed to generate pdf");
+fn open_file() -> Result<File, String> {
+    let open = File::open("file.jpeg");
+    match open {
+        Ok(file) => Ok(file),
+        Err(e) => Err(e.to_string()),
+    }
 }
