8e85a3583faca817c72ed02f589f58fb7fc413fb
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-26 15:24:30 +0600

    if expressions & if late Some

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,11 +1,14 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let v = (2, -2);
-    match v {
-        (x, y) if x == y => println!("These are twins"),
-        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
-        (x, _) if x % 2 == 1 => println!("The first one is odd"),
-        _ => println!("No correlation..."),
+    let mut x = 10;
+    x = if x % 2 == 0 { x / 2 } else { x / 3 + 1 };
+    println!("{}", x);
+
+    let arg = std::env::args().next();
+    if let Some(v) = arg {
+        println!("v is : {}", v) // ? v is : target/debug/rust-playground
+    } else {
+        println!("Missing name?");
     }
 }
