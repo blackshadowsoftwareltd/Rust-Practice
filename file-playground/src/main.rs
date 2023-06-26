0b1964a0b7de87e906aec81d35d5b7a75dd5e412
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-26 15:44:35 +0600

    error handling using ErrorKind

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -1,5 +1,6 @@
 #![deny(clippy::all)]
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let result = open_file();
@@ -14,6 +15,12 @@ fn open_file() -> Result<File, String> {
     let open = File::open("file.jpeg");
     match open {
         Ok(file) => Ok(file),
-        Err(e) => Err(e.to_string()),
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => Err("File Not Found.".to_string()),
+            ErrorKind::PermissionDenied => Err("Permission Denied.".to_string()),
+            ErrorKind::Unsupported => Err("Unsupported.".to_string()),
+            ErrorKind::Other => Err("Other".to_string()),
+            _ => Err("Unknown Error".to_string()),
+        },
     }
 }
