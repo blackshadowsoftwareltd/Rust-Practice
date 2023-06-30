2b5e5174a3e991c762e9cc000398d48ed742e5f9
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-30 15:17:53 +0600

    ? operator in rust

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,14 +1,15 @@
 #![deny(clippy::all)]
 
-fn main() {
-    let v = vec![10, 20, 30];
+fn main() -> Result<(), String> {
+    let result = divide(10, 2)?;
+    println!("Result: {}", result);
+    Ok(())
+}
 
-    for x in v {
-        println!("x: {x}");
-    }
-
-    // ? step_by :: Creates an iterator starting at the same point, but stepping by the given amount at each iteration.
-    for i in (1..100).step_by(10) {
-        println!("i : {}", i)
+fn divide(a: i32, b: i32) -> Result<i32, String> {
+    if b == 0 {
+        Err("Division by zero".to_string())
+    } else {
+        Ok(a / b)
     }
 }
