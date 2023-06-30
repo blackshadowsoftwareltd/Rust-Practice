012a72f69a101f8420ead38cb590e393f7799c67
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-01 02:30:53 +0600

    ternary operator in rust with unwrap

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,15 +1,15 @@
 #![deny(clippy::all)]
 
-fn main() -> Result<(), String> {
-    let result = divide(10, 2)?;
-    println!("Result: {}", result);
-    Ok(())
-}
-
-fn divide(a: i32, b: i32) -> Result<i32, String> {
-    if b == 0 {
-        Err("Division by zero".to_string())
-    } else {
-        Ok(a / b)
+fn main() {
+    let mut numbers: Vec<i32> = Vec::new();
+    {
+        let condition = !numbers.is_empty() && numbers.first().is_some();
+        let first = if condition {
+            numbers.first().unwrap()
+        } else {
+            &0
+        };
+        println!("{:?}", first);
     }
+    numbers.push(0);
 }
