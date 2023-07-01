c9f2be293ec5a6c632631a55363022bbbcfec39c
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-01 21:52:04 +0600

    async example

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,15 +1,15 @@
 #![deny(clippy::all)]
 
-fn main() {
-    let mut numbers: Vec<i32> = Vec::new();
-    {
-        let condition = !numbers.is_empty() && numbers.first().is_some();
-        let first = if condition {
-            numbers.first().unwrap()
-        } else {
-            &0
-        };
-        println!("{:?}", first);
-    }
-    numbers.push(0);
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    let first = apis("Data 1".to_string()).await;
+    println!("{:?}", first);
+    let second = apis("Data 2".to_string()).await;
+    println!("{:?}", second);
+}
+async fn apis(data: String) -> String {
+    sleep(Duration::from_secs(2)).await;
+    data
 }
