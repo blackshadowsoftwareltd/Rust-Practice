4019ce49cafba32b3f3bed06e93db29420e925fb
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-03 10:40:17 +0600

    capacity in vec

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,17 +1,28 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut dummy_text = String::new();
-    dummy_text = String::from("World");
-    dummy_text.push_str("Hello");
-    println!("{:?}", dummy_text);
+    let mut vec1: Vec<i32> = Vec::new();
+    vec1.push(10);
 
-    let mut text = String::with_capacity(dummy_text.len());
-    println!("{:?}", text.len());
+    let mut vec2 = Vec::with_capacity(vec1.len());
+    vec2.push(20);
+    println!("capacity {:?}", vec2.capacity());
 
-    println!("{:?}", dummy_text);
+    vec2.extend(vec1.clone().iter());
+    println!(
+        "vec : {:?}, capacity : {:?}, len : {:?}",
+        vec2,
+        vec2.capacity(),
+        vec2.len()
+    ); //? vec : [20, 10], capacity : 4, len : 2
 
-    text = dummy_text;
-
-    println!("{:?}", text);
+    vec1 = vec2.clone();
+    vec1.push(50);
+    vec2.extend(vec1.iter());
+    println!(
+        "vec : {:?}, capacity : {:?}, len : {:?}",
+        vec2,
+        vec2.capacity(),
+        vec2.len()
+    ); //? vec : [20, 10, 20, 10, 50], capacity : 8, len : 5
 }
