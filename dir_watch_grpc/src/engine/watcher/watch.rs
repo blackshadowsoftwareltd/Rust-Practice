4299905745a7c7c116aedf5def0b39c3564f7799
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-03 10:41:51 +0600

    removed dir watcher, recently created a new repo for dir watcher

--- dir_watch_grpc/src/engine/watcher/watch.rs
+++
@@ -1,1 +0,0 @@
-pub fn watch_dir(path: String) {}
--- dir_watch_grpc/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![deny(clippy::all)]
-
-pub mod engine;
-use engine::watcher::watch::watch_dir;
-
-fn main() {
-    watch_dir("path".to_string());
-}
