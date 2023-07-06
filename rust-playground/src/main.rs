fd4ae4f3c2f68fb2cf6489cab2f413e8b0015bda
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-07-06 22:37:13 +0600

    copy & clone in rust

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,15 +1,42 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    println!("vec : {:?}", vec1);
+    let player = Player {
+        player_name: PlayerName {
+            name: String::from("Lionel Messi"),
+        },
+        jersey: PlayerJersey {
+            number: 10,
+            size: 5,
+        },
+    };
+    println!("=> Player: {:?}", player);
 
-    vec1.retain(|x| x % 2 == 0);
-    println!("vec : {:?}", vec1); // ? vec : [2, 4, 6, 8, 10]
+    let jersey = player.jersey;
+    println!("=> Player Jersey: {:?}", jersey);
 
-    vec1 = vec![1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 10];
-    println!("vec : {:?}", vec1); // ? vec : [1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 10]
+    let player_name = &player.player_name; // ! we must use & to borrow the value.
+    println!("=> Player Name: {:?}", player_name);
 
-    vec1.dedup(); //? Remove consecutive duplicates.
-    println!("vec : {:?}", vec1); // ? vec : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    println!("=> Player: {:?}", player); // ! other wise this line will be throw error : borrow of partially moved value: `player`. partial move occurs because `player.player_name` has type `PlayerName`,
+
+    let player_name = player.player_name.clone(); //? no need & ref if we clone it
+    println!("=> Player Name: {:?}", player_name);
+    println!("=> Player: {:?}", player);
+}
+#[derive(Debug, Clone)] // ! Copy [the trait `Copy` cannot be implemented for this type]
+struct Player {
+    player_name: PlayerName,
+    jersey: PlayerJersey,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PlayerJersey {
+    number: u8,
+    size: u8,
+}
+
+#[derive(Debug, Clone)] // ! Copy [the trait `Copy` cannot be implemented for this type]
+struct PlayerName {
+    name: String,
 }
