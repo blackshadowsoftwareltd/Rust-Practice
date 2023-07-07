7a79560d2a11a82faa81eb0e7c84c3d84ecc8b37
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-07-07 23:15:52 +0600

    Method Receiver

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,13 +1,51 @@
 #![deny(clippy::all)]
-#[repr(u32)]
-enum Bar {
-    A, // 0
-    B = 10000,
-    C, // 10001
+fn main() {
+    let mut race = Race::new("Monaco Grand Prix");
+    race.add_lap(70);
+    race.add_lap(68);
+    race.print_laps();
+    race.add_lap(71);
+    race.print_laps();
+    race.finish();
+    // race.add_lap(42); // ? error: cannot borrow `race` as mutable because it is also borrowed as immutable
 }
 
-fn main() {
-    println!("A: {}", Bar::A as u32); // ? A: 0
-    println!("B: {}", Bar::B as u32); // ? B: 10000
-    println!("C: {}", Bar::C as u32); // ? C: 10001
+#[derive(Debug)]
+struct Race {
+    name: String,
+    laps: Vec<i32>,
 }
+
+impl Race {
+    fn new(name: &str) -> Race {
+        // No receiver, a static method
+        Race {
+            name: String::from(name),
+            laps: Vec::new(),
+        }
+    }
+
+    fn add_lap(&mut self, lap: i32) {
+        // Exclusive borrowed read-write access to self
+        self.laps.push(lap);
+    }
+
+    fn print_laps(&self) {
+        // Shared and read-only borrowed access to self
+        println!("Recorded {} laps for {}:", self.laps.len(), self.name);
+        for (idx, lap) in self.laps.iter().enumerate() {
+            println!("Lap {idx}: {lap} sec");
+        }
+    }
+
+    fn finish(self) {
+        // Exclusive ownership of self
+        let total = self.laps.iter().sum::<i32>();
+        println!("Race {} is finished, total lap time: {}", self.name, total);
+    }
+}
+
+/*
+iter() returns an iterator that allows you to iterate over the collection by reference. This means that you can only access the elements of the collection through references.
+into_iter() returns an iterator that allows you to iterate over the collection by value. This means that you can take ownership of the elements of the collection and move them into other places.
+*/
