216aba5651440c665bad44a23e3933ca24d1f150
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-07-15 10:21:38 +0600

    Mutable Parallelism

--- thread_playground/src/main.rs
+++ thread_playground/src/main.rs
@@ -1,26 +1,9 @@
 #![deny(clippy::all)]
 
-use std::{thread::sleep, time::Duration};
-
 use rayon::prelude::*;
 #[tokio::main]
 async fn main() {
-    let list: Vec<String> = vec![
-        "Hello".to_string(),
-        "World".to_string(),
-        "This".to_string(),
-        "is".to_string(),
-        "a".to_string(),
-        "test".to_string(),
-    ];
-    // do_something(&list).await;
-    do_something(&list).await;
-}
-
-async fn do_something(list: &[String]) {
-    list.par_iter().for_each(|x| print_data(x));
-}
-fn print_data(v: &String) {
-    sleep(Duration::from_secs(2));
-    println!("{:?}", v)
+    let mut list = vec![1, 2, 3, 4, 5, 6, 7];
+    list.par_iter_mut().for_each(|v| *v += 10);
+    println!("{:?}", list);
 }
