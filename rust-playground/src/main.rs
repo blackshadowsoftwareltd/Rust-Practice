2570e284dc05bb8680b3c3c55c8427e25816b146
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-07-21 11:27:56 +0600

    generics impl

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,9 +1,21 @@
 #![deny(clippy::all)]
+
+use std::fmt::Debug;
 fn main() {
-    let p = Point { x: 5, y: 10.0 };
-    println!("p.x = {}, p.y = {}", p.x, p.y);
+    let p = Point::new(5, 10.0);
+    p.print_data();
 }
+#[derive(Debug)]
 struct Point<T> {
     x: T,
     y: f32,
+}
+
+impl<T: Debug> Point<T> {
+    fn new(x: T, y: f32) -> Self {
+        Self { x, y }
+    }
+    fn print_data(&self) {
+        println!("x : {:?}, y : {:?}", self.x, self.y);
+    }
 }
