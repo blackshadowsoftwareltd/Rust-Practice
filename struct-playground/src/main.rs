fb6fd8a1d9ea7111792506be5df8b58a1bc23d84
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-23 19:44:15 +0600

    equality checking by trait & struct

--- struct-playground/src/main.rs
+++ struct-playground/src/main.rs
@@ -1,20 +1,25 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let p1 = Player::default();
-    let p2 = p1.clone();
-    println!(
-        "IS {:?}\nEqual to {:?}?The answer is {:?}",
-        &p1,
-        &p2,
-        if p1 == p2 { "Yes" } else { "No" }
-    );
+    let a = Centimeter(10);
+    let b = Centimeter(20);
+    println!("{:?} == {:?} ? {:?}", a, b, a.equal(&b));
+    println!("{:?} != {:?} ? {:?}", a, b, a.not_equal(&b));
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Default)]
+trait Equals {
+    fn equal(&self, other: &Self) -> bool;
 
-struct Player {
-    name: String,
-    strength: u8,
-    hit_points: u8,
+    fn not_equal(&self, other: &Self) -> bool {
+        !self.equal(other)
+    }
+}
+
+#[derive(Debug)]
+struct Centimeter(i16);
+
+impl Equals for Centimeter {
+    fn equal(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
 }
