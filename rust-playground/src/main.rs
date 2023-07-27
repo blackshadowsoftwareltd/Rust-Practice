d6677fb9497d86c806adba0b87e2e5bc6529d5f4
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-07-27 16:03:05 +0600

    Types implement From and Into

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,12 +1,17 @@
 #![deny(clippy::all)]
-use std::fmt::Display;
 
 fn main() {
-    let name = "Alice";
-    let name = get_name(name);
-    println!("{}", name);
-}
+    //? Types implement From to facilitate type conversions
+    let s = String::from("hello world");
+    let addr = std::net::Ipv4Addr::from([127, 0, 0, 1]);
+    let one = i16::from(true);
+    let bigger = i32::from(123i16);
+    println!("{s}, {addr}, {one}, {bigger}");
 
-fn get_name(name: impl Display) -> impl Display {
-    format!("{} is a position", name)
+    //? Types implement Into to facilitate type conversions
+    let s: String = "hello world".into();
+    let addr: std::net::Ipv4Addr = [127, 0, 0, 1].into();
+    let one: i16 = true.into();
+    let bigger: i32 = 123i16.into();
+    println!("{s}, {addr}, {one}, {bigger}");
 }
