8409407c23812250e039c4d9e39b4e90aa7483de
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-07-30 20:08:46 +0600

    Drop Trait

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,17 +1,26 @@
 #![deny(clippy::all)]
 
-fn main() {
-    //? Types implement From to facilitate type conversions
-    let s = String::from("hello world");
-    let addr = std::net::Ipv4Addr::from([127, 0, 0, 1]);
-    let one = i16::from(true);
-    let bigger = i32::from(123i16);
-    println!("{s}, {addr}, {one}, {bigger}");
+struct Droppable {
+    name: &'static str,
+}
 
-    //? Types implement Into to facilitate type conversions
-    let s: String = "hello world".into();
-    let addr: std::net::Ipv4Addr = [127, 0, 0, 1].into();
-    let one: i16 = true.into();
-    let bigger: i32 = 123i16.into();
-    println!("{s}, {addr}, {one}, {bigger}");
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.name);
+    }
+}
+
+fn main() {
+    let a = Droppable { name: "a" };
+    {
+        let _b = Droppable { name: "b" };
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+            println!("Exiting block B");
+        }
+        println!("Exiting block A");
+    }
+    drop(a);
+    println!("Exiting main");
 }
