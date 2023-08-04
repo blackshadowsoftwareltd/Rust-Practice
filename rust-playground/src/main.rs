6a5ec4450ceba1fbe6367c60da62f8850c732b6c
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-04 18:15:29 +0600

    default

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,26 +1,33 @@
 #![deny(clippy::all)]
 
-struct Droppable {
-    name: &'static str,
-}
-
-impl Drop for Droppable {
-    fn drop(&mut self) {
-        println!("Dropping {}", self.name);
-    }
-}
-
+//  Ternary Operator
 fn main() {
-    let a = Droppable { name: "a" };
-    {
-        let _b = Droppable { name: "b" };
-        {
-            let _c = Droppable { name: "c" };
-            let _d = Droppable { name: "d" };
-            println!("Exiting block B");
-        }
-        println!("Exiting block A");
+    let defauld_struct = Derived::default();
+    println!("default_struct : {:?}", defauld_struct);
+
+    let almost_default_struct = Derived {
+        x: 0,
+        y: "Y is set".into(),
+        ..Default::default()
+    };
+    println!("almost_default_struct : {:?}", almost_default_struct);
+
+    let nothing: Option<Derived> = None;
+    println!("nothing : {:#?}", nothing);
+}
+
+#[derive(Debug)]
+struct Implemented(String);
+
+#[derive(Debug, Default)]
+struct Derived {
+    x: u8,
+    y: String,
+    z: Implemented,
+}
+
+impl Default for Implemented {
+    fn default() -> Self {
+        Self("default".into())
     }
-    drop(a);
-    println!("Exiting main");
 }
