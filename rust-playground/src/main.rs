5ba9cd4477d25fac937b267ce04a2ffb42b4a0a8
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-04 19:04:07 +0600

    Closures

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,27 +1,13 @@
 #![deny(clippy::all)]
 
-use std::ops::Add;
-
 fn main() {
-    let p1 = Point { x: 10, y: 20 };
-    let p2 = Point { x: 100, y: 200 };
-    let p3 = p1.add(p2);
-    println!("Points :: {:?} +{:?} +{:?}", p1, p2, p3);
+    let outer_value = 5;
+    //?regular function can't refer to variables in the enclosing environment
+    let v = |x| x + outer_value;
+    apply_with_log(v, 10);
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl std::ops::Add for Point {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
+fn apply_with_log(func: impl FnOnce(i32) -> i32, input: i32) -> i32 {
+    print!("Calling function for {:?}", input);
+    func(input)
 }
