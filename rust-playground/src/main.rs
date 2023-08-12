3b0383072e6292498f8a94c3cc7bb4e4cb35a745
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-12 18:02:57 +0600

    Closures -> Type anonymity

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,50 +1,14 @@
 #![deny(clippy::all)]
-
-use std::mem;
-
 fn main() {
-    let greeting = "hello";
-    let mut farewell = "goodbye".to_owned();
+    let x = 5;
+    let print = || println!("x : {:?}", x);
 
-    let diary = || {
-        println!("I said {}", greeting);
-        farewell.push_str("!!!");
-        println!("Then I screamed {}.", farewell);
-        println!("Now I can sleep. zzzzz");
-        mem::drop(farewell);
-    };
-
-    // ---------------------------------------
-    apply(diary);
-
-    let double = |x| 2 * x;
-    let applied_to_3 = apply_to_3(double);
-    println!("3 doubled : {}", applied_to_3);
-
-    // ---------------------------------------
-    let mut x = 4;
-    let add_to_2 = || x += 2;
-    make_twice(add_to_2);
-    println!("twice : {}", x);
+    apply(print);
 }
 
-fn apply<F>(func: F)
+fn apply<F>(f: F)
 where
-    F: FnOnce(),
+    F: Fn(),
 {
-    func();
-}
-
-fn apply_to_3<F>(func: F) -> i32
-where
-    F: Fn(i32) -> i32,
-{
-    func(3)
-}
-
-fn make_twice<F>(mut func: F)
-where
-    F: FnMut(),
-{
-    func();
+    f();
 }
