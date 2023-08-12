3de9b322f78eebc272d0e7803ca9a4c0355aeb45
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-12 18:36:08 +0600

    Closures -> Iterator::any

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,25 +1,34 @@
 #![deny(clippy::all)]
 fn main() {
-    let fn_plain = create_fn();
-    let mut fn_mut = create_fn_mut();
-    let fn_once = create_fn_once();
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
 
-    fn_plain();
-    fn_mut();
-    fn_once();
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+
+    println!("vec1 len: {}", vec1.len());
+    println!("First element of vec1 is: {}", vec1[0]);
+
+    //? `into_iter()` does move `vec2` and its elements, so they cannot be used again
+    // println!("First element of vec2 is: {}", vec2[0]);
+    // println!("vec2 len: {}", vec2.len());
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    println!("2 in array2: {}", array2.into_iter().any(|x| x == 2));
+    println!("First element of array2 is: {}", array2[0]);
+    println!("array2 len: {}", array2.len());
 }
 
-fn create_fn() -> impl Fn() {
-    let text = "Fn".to_owned();
-    move || println!("This is a : {:?}", text)
-}
+//? Iterator::any work look like
+pub trait Iterator {
+    type Item; //? The type being iterated over.
 
-fn create_fn_mut() -> impl FnMut() {
-    let text = "Fn Mut".to_owned();
-    move || println!("This is a : {:?}", text)
-}
-
-fn create_fn_once() -> impl FnOnce() {
-    let text = "Fn Once".to_owned();
-    move || println!("This is a : {:?}", text)
+    // fn any_z<F: FnMut(Self::Item)>(&mut self, f: F) -> bool;
+    fn any<F>(&mut self, f: F) -> bool
+    //? `any` takes `&mut self` meaning the caller may be borrowed  and modified, but not consumed.
+    where
+        F: FnMut(Self::Item) -> bool;
 }
