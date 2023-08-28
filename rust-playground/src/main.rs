5252fa6922ca8ac14b45fe1a8bb90b4c6b89bff7
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-08-28 11:33:07 +0600

    once lock with with update data.

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,10 +1,32 @@
 #![deny(clippy::all)]
-use chrono::prelude::*;
+
+use std::{
+    collections::HashSet,
+    sync::{Mutex, OnceLock},
+};
+pub static INFO: OnceLock<Mutex<HashSet<i32>>> = OnceLock::new();
+
 fn main() {
-    let utc: DateTime<Utc> = Utc::now();
-    println!("{:?}", utc);
-    let utc_string = utc.to_string();
-    println!("{}", utc_string);
-    let f = utc_string.parse::<DateTime<Utc>>().unwrap();
-    println!("time : {:?}", f);
+    init();
+    let v = INFO.get().unwrap().lock().unwrap().clone();
+    println!("{:?}", v);
+
+    INFO.get().unwrap().lock().unwrap().insert(6);
+    let v = INFO.get().unwrap().lock().unwrap().clone();
+    println!("{:?}", v);
+}
+
+fn init() {
+    let mut info = INFO
+        .get_or_init(|| {
+            let mut set = HashSet::new();
+            set.insert(1);
+            set.insert(2);
+            set.insert(3);
+            Mutex::new(set)
+        })
+        .lock()
+        .unwrap();
+    info.insert(4);
+    info.insert(5);
 }
