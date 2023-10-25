b9b593bb3078da40bd473453a5511ff700aed941
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-10-25 18:31:49 +0600

    create nested dir

--- file-playground/src/main.rs
+++ file-playground/src/main.rs
@@ -1,16 +1,13 @@
 #![deny(clippy::all)]
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf};
 
 fn main() {
-    let path = PathBuf::from("files/pxfuel.jpg");
-    let mb = file_size_in_mb(path).unwrap();
-    println!("mb : {:?}", mb);
-}
+    let path = PathBuf::from("/home/remon/Desktop")
+        .join("Root")
+        .join("Child")
+        .join("text.txt");
 
-fn file_size_in_mb(path: PathBuf) -> Result<f64, String> {
-    if !path.exists() {
-        return Err("Error : File not Exist".to_string());
-    }
-    let bytes = std::fs::metadata(path).unwrap().len();
-    Ok((bytes as f64 / 1024.0) / 1024.0)
+    std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+
+    File::create(path).unwrap();
 }
