6387a895c19f5d070780721fcaf595b6075f169e
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-12-14 14:46:29 +0600

    skip field if null in serializeing

--- json-playground/src/main.rs
+++ json-playground/src/main.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = Person {
+        name: String::from("John"),
+        age: 30,
+        height: Some(180),
+        weight: None,
+        gender: Some("Male".to_string()),
+    };
+    let json = serde_json::to_string(&user1).unwrap();
+    println!("{}", json);
+    let data = serde_json::from_str::<Person>(&json).unwrap();
+    println!("{:?}", data.clone());
+    println!("{:?}", data.gender)
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Person {
+    name: String,
+    age: u32,
+    height: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")] // skip this field when serializing if it is None
+    weight: Option<u32>,
+    #[serde(skip)] // skip this field when serializing and deserializing
+    gender: Option<String>,
 }
