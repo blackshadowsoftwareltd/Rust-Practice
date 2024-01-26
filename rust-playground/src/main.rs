aa0f6819ed8ecf3e82353ed08d4ee527cd1a7de3
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-26 10:00:27 +0600

    log4rs

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,17 +1,39 @@
 #![deny(clippy::all)]
 
+use log::{debug, error, info, warn, LevelFilter};
+use log4rs::{
+    append::{console::ConsoleAppender, file::FileAppender},
+    config::{Appender, Logger, Root},
+    encode::pattern::PatternEncoder,
+    Config,
+};
+
 fn main() {
-    let both_int = Point { x: 2, y: 4 };
-    let both_float = Point { x: 1.2, y: 3.4 };
-    let int_float = Point { x: 5, y: 10.8 };
-    println!(
-        "boath int : {:?}, both float : {:?}, int & float : {:?}",
-        both_int, both_float, int_float
-    );
+    log_config();
+
+    debug!("Mary has a little lamb");
+    error!("{}", "Its fleece was white as snow");
+    info!("{:?}", "And every where that Mary went");
+    warn!("{:#?}", "The lamb was sure to go");
 }
 
-#[derive(Debug)]
-struct Point<X, Y> {
-    x: X,
-    y: Y,
+fn log_config() {
+    let stdout = ConsoleAppender::builder().build();
+    let requests = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
+        .build("log/requests.log")
+        .unwrap();
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .appender(Appender::builder().build("requests", Box::new(requests)))
+        .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
+        .logger(
+            Logger::builder()
+                .appender("requests")
+                .additive(false)
+                .build("app::requests", LevelFilter::Info),
+        )
+        .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
+        .unwrap();
+    let _ = log4rs::init_config(config).unwrap();
 }
