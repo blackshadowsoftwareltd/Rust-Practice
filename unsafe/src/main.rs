7879d33271d18dcd5842ba58ce65d9c2ea022ce4
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-07-04 09:43:15 +0600

    pointer

--- unsafe/src/main.rs
+++ unsafe/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn main() {
+    let mut number = 5;
+    let r1 = &number as *const i32;
+    let r2 = &mut number as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+}
